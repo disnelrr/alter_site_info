90ae3dd252790bfab8d91866279b1327ba484efc
Disnel Rodriguez Rodriguez <disnelrr@gmail.com>
2021-01-07 18:05:48 -0500

    Added files

---
+++ src/Controller/Controller.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace Drupal\alter_site_info\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * Simple Controller to check if correct Site API key is passed in as a parameter,
+ * along with a node identifier.
+ */
+class Controller extends ControllerBase {
+  /**
+   * Returns a json representation of a given page if Site API Key is provided.
+   */
+  public function content($apikey, $nid, Request $request) {
+    $nid = $request->attributes->get('_raw_variables')->get('nid');
+    $argapikey = $request->attributes->get('_raw_variables')->get('apikey');
+    $node = Node::load($nid);
+    $sisapikey = \Drupal::config('system.site')->get('siteapikey');
+    if ( $sisapikey == $argapikey ) {
+      return [
+        '#theme' => 'alter_site_info_json_page',
+        '#apikey' => $message,
+        '#nid' => $nid,
+        '#node' => \Drupal::service('serializer')->serialize($node, 'json'),
+      ];
+    } else {
+       $build = [
+        '#markup' => $this->t('Access Denied!'),
+      ];
+      return $build;
+    }
+  }
+
+} 
---
+++ src/Form/NewSiteInformationForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\alter_site_info\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\system\Form\SiteInformationForm;
+
+
+class NewSiteInformationForm extends SiteInformationForm {
+ 
+   /**
+   * {@inheritdoc}
+   */
+      public function buildForm(array $form, FormStateInterface $form_state) {
+        $site_config = $this->config('system.site');
+        $form =  parent::buildForm($form, $form_state);
+        $form['site_information']['siteapikey'] = [
+            '#type' => 'textfield',
+            '#title' => t('Site API Key'),
+            '#default_value' => $site_config->get('siteapikey') ?: 'No API Key yet',
+            '#description' => t("Custom field to set the API Key"),
+        ];
+
+        // Change form submit button text to 'Update Configuration'
+        $form['actions']['submit']['#value'] = t('Update configuration');
+        
+        return $form;
+    }
+    
+      public function submitForm(array &$form, FormStateInterface $form_state) {
+        $this->config('system.site')
+          ->set('siteapikey', $form_state->getValue('siteapikey'))
+          ->save();
+        parent::submitForm($form, $form_state);
+      }
+} 
